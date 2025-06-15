49d962cfe8594469892b68b837c908c8e1d400bb
ToniBirat7 <gotofreelancing1@gmail.com>
2025-06-15 21:28:03 +0545

    Continue the Fs write Tomorrow

--- Day3_Codev/fs_module/index.js
+++ Day3_Codev/fs_module/index.js
@@ -1,5 +1,19 @@
 const fs = require("node:fs");
 
+console.log("Synchronous");
+
 const content = fs.readFileSync("./file.txt", "utf-8"); // Sync
 
 console.log(content);
+
+console.log("Synchronous");
+
+const contentSync = fs.readFile("./file.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
+
+console.log("Last");
